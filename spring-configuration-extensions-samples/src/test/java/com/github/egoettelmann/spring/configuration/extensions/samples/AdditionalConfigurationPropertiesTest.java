c29583dcb406ba65f5f0de9894fab7a1a0c2193c
Elio Goettelmann <elio.goettelmann@gmail.com>
2022-09-06 23:01:56 +0200

    Tests are now using Json-Patch

---
+++ spring-configuration-extensions-samples/src/test/java/com/github/egoettelmann/spring/configuration/extensions/samples/AdditionalConfigurationPropertiesTest.java
@@ -0,0 +1,31 @@
+package com.github.egoettelmann.spring.configuration.extensions.samples;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.diff.JsonDiff;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+
+public class AdditionalConfigurationPropertiesTest {
+
+    @Test
+    public void testAdditionalConfigurationProperties() throws Exception {
+        final File generatedFile = ResourceUtils.getFile("classpath:META-INF/additional-spring-configuration-metadata.json");
+        Assertions.assertTrue(generatedFile.exists(), "Additional configuration metadata file should exist");
+        final JsonNode generatedJsonNode = new ObjectMapper().readTree(generatedFile);
+        Assertions.assertTrue(generatedJsonNode.has("properties"), "No field 'properties' found");
+        final JsonNode generatedProperties = generatedJsonNode.get("properties");
+
+        final File expectedFile = ResourceUtils.getFile("classpath:additional-spring-configuration-metadata-test-1.json");
+        final JsonNode expectedJsonNode = new ObjectMapper().readTree(expectedFile);
+        final JsonNode expectedProperties = expectedJsonNode.get("properties");
+
+        final JsonNode jsonPatch = JsonDiff.asJson(expectedProperties, generatedProperties);
+        Assertions.assertTrue(jsonPatch.isArray(), "Patch should be an array");
+        Assertions.assertEquals(0, jsonPatch.size(), "Path should be empty");
+    }
+
+}
